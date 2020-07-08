11b8e836e8f1f9232a83f8b4ae9d93c19695e928
Joel Tanzi <jtanziredux@gmail.com>
2020-07-07 20:53:41 -0500

    Scaffolding and Task, TaskList components

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ["../src/app/components/**/*.stories.ts"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-notes",
+  ],
+};
